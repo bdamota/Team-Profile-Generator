de9fe19919d92968464e131fc78d48fb950aa793
LYNN DAMOTA <bdamota0506@gmail.com>
2020-07-24 23:38:24 -0700

    test revised for manager

--- _ _ tests _ _/Manager.test.js
+++ _ _ tests _ _/Manager.test.js
@@ -10,30 +10,13 @@ test('creates an employee object', () => {
   });
 
 
-  test("gets manager's name", () => {
-    const manager= new Manager('John', '25', '2', 'john@john.com');
-
-    expect(manager.getName()).toEqual(expect.stringContaining(manager.name));
-  });
-
-  test("gets manager's id", () => {
-    const manager= new Manager('John', '25', '2', 'john@john.com');
-
-    expect(manager.getID()).toEqual(expect.stringContaining(manager.id));
-  });
-
   test("gets manager's office number", () => {
     const manager = new Manager('John', '25', '2', 'john@john.com');
 
     expect(manager.getEmail()).toEqual(expect.stringContaining(manager.office));
   });
 
-  test("gets manager's email", () => {
-    const manager = new Manager('John', '25', '2', 'john@john.com');
-
-    expect(manager.getEmail()).toEqual(expect.stringContaining(manager.email));
-  });
-
+ 
   test("gets manager's role", () => {
     const manager = new Manager('John', '25', '2', 'john@john.com');
 
