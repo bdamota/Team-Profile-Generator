a5f9520732e13f77537fadd19121d97effd39711
LYNN DAMOTA <bdamota0506@gmail.com>
2020-07-25 20:40:15 -0700

    style added for final html

--- src/page-template.js
+++ src/page-template.js
@@ -3,14 +3,14 @@ const fs = require('fs');
 // create manager section
 const generateManager = Manager => {
  return `
-    <div class="card" style="width: 18rem;">
-     <h5 class="card-title">${Manager.name}</h5> 
-     <h5 class="card-text">${Manager.role}</h5>  
+    <div class="card cardA" style="width: 20rem;">
+     <h4 class="card-header">${Manager.name}</h4> 
+     <h5 class="card-header">📋${Manager.role}</h5>  
         <div class="card-body">
             <ul class="list-group list-group-flush">
                 <li class="list-group-item">ID: ${Manager.id}</li>
                 <li class="list-group-item">Email: <a href="mailto:${Manager.email}">${Manager.email}</a></li>
-                <li class="list-group-item">Office number: ${Manager.officeNumber}</li>
+                <li class="list-group-item">Office Number: ${Manager.office}</li>
             </ul>  
         </div>
     </div>
@@ -21,9 +21,9 @@ const generateEngineers = engineersArr => {
       ${engineersArr
         .map(({ name, id, email, github, role }) => {
           return `
-          <div class="card" style="width: 18rem;">
-          <h5 class="card-title">${name}</h5> 
-          <h5 class="card-text">${role}</h5>  
+          <div class="card cardB" style="width: 20rem;">
+          <h4 class="card-header">${name}</h4> 
+          <h5 class="card-header">👓${role}</h5>  
              <div class="card-body">
                     <ul class="list-group list-group-flush">
                     <li class="list-group-item">ID: ${id}</li>
@@ -41,9 +41,9 @@ const generateInterns = internsArr => {
       ${internsArr
         .map(({ name, id, email, school, role }) => {
           return `
-          <div class="card" style="width: 18rem;">
-          <h5 class="card-title">${name}</h5> 
-          <h5 class="card-text">${role}</h5>  
+          <div class="card cardC" style="width: 20rem;">
+          <h4 class="card-header">${name}</h4> 
+          <h5 class="card-header">🎓${role}</h5>  
              <div class="card-body">
                     <ul class="list-group list-group-flush">
                     <li class="list-group-item">ID: ${id}</li>
@@ -74,20 +74,21 @@ module.exports = templateData => {
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
     <link href="https://fonts.googleapis.com/css?family=Public+Sans:300i,300,500&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="./style.css">
   </head>
 
   <body>
     <header>
-      <div class="container flex-row justify-space-between align-center py-3">
-        <h1 class="page-title text-secondary bg-dark py-2 px-3">My Team</h1>
-
-      </div>
+        <nav>
+            <div class="container flex-row justify-space-between align-center py-3">
+                <h1 class="page-title text-secondary bg-dark py-2 px-3">My Team</h1>
+            </div>
+        </nav> 
     </header>
     <main class="container my-5">
-   ${generateManager(manager)}
-   ${generateEngineers(engineers)}
-   ${generateInterns(interns)}
+        ${generateManager(manager)}
+        ${generateEngineers(engineers)}
+        ${generateInterns(interns)}
     </main>
   </body>
   </html>
